ce274644474eb30cc32a80865380669cc4b5ee20
dalimisonowal <dalimisonowal33@gmail.com>
2025-02-14 01:01:45 +0530

    Force install seaborn at runtime

--- quant_dashboard.py
+++ quant_dashboard.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.append('/home/appuser/.local/lib/python3.12/site-packages')
+import os
+os.system("pip install seaborn --no-cache-dir --user")
 import streamlit as st
 import numpy as np
 import pandas as pd
